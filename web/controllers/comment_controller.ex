ad61815f7bc4ec3c004db4d5093d17bd4b332a02
Philip Alekseev <phil.alekseev@gmail.com>
2017-02-05 18:04:43 -0500

    Hook up delete and approve for comments.

--- web/controllers/comment_controller.ex
+++ web/controllers/comment_controller.ex
@@ -22,6 +22,32 @@ defmodule Folklore.CommentController do
     end
   end
 
-  def update(conn, _), do: conn
-  def delete(conn, _), do: conn
+  def update(conn, %{"id" => id, "post_id" => post_id, "comment" => comment_params}) do
+    post = Repo.get!(Post, post_id)
+    |> Repo.preload(:user)
+    comment = Repo.get!(Comment, id)
+    changeset = Comment.changeset(comment, comment_params)
+
+    case Repo.update(changeset) do
+      {:ok, _} ->
+        conn
+        |> put_flash(:info, "Comment updated successfully.")
+        |> redirect(to: user_post_path(conn, :show, post.user, post))
+      {:error, _} ->
+        conn
+        |> put_flash(:info, "Failed to update comment.")
+        |> redirect(to: user_post_path(conn, :show, post.user, post))
+    end
+  end
+
+  def delete(conn, %{"id" => id, "post_id" => post_id}) do
+    post = Repo.get!(Post, post_id)
+    |> Repo.preload(:user)
+    Repo.get!(Comment, id)
+    |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Deleted comment.")
+    |> redirect(to: user_post_path(conn, :show, post.user, post))
+  end
 end
