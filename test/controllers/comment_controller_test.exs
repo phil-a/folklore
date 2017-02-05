2595e67e9eb891f5eac78861ac53666d5e688100
Philip Alekseev <phil.alekseev@gmail.com>
2017-02-05 18:14:40 -0500

    Add authorization to comments controller.

--- test/controllers/comment_controller_test.exs
+++ test/controllers/comment_controller_test.exs
@@ -27,8 +27,8 @@ defmodule Folklore.CommentControllerTest do
     assert html_response(conn, 200) =~ "Oops, something went wrong"
   end
 
-  test "deletes the comment", %{conn: conn, post: post, comment: comment} do
-    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+  test "deletes the comment when logged in as an authorized user", %{conn: conn, user: user, post: post, comment: comment} do
+    conn = login_user(conn, user) |> delete(post_comment_path(conn, :delete, post, comment))
     assert redirected_to(conn) == user_post_path(conn, :show, post.user, post)
     refute Repo.get(Comment, comment.id)
   end
@@ -37,6 +37,18 @@ defmodule Folklore.CommentControllerTest do
     conn = login_user(conn, user) |> put(post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
     assert redirected_to(conn) == user_post_path(conn, :show, user, post)
     assert Repo.get_by(Comment, %{id: comment.id, approved: true})
+  end
+
+  test "does not delete the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = delete(conn, post_comment_path(conn, :delete, post, comment))
+    assert redirected_to(conn) == page_path(conn, :index)
+    assert Repo.get(Comment, comment.id)
+  end
+
+  test "does not update the comment when not logged in as an authorized user", %{conn: conn, post: post, comment: comment} do
+    conn = put(conn, post_comment_path(conn, :update, post, comment), comment: %{"approved" => true})
+    assert redirected_to(conn) == page_path(conn, :index)
+    refute Repo.get_by(Comment, %{id: comment.id, approved: true})
   end
 
 end
