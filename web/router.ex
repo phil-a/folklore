aabb96e671aa30bb46e8962b6005d42347abb789
Philip Alekseev <phil.alekseev@gmail.com>
2017-02-12 19:11:54 -0500

    Add current user plug

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Folklore.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Folklore.CurrentUserPlug
   end
 
   pipeline :api do
